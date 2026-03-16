8fffc0704f16ba3e9abec3f3883c00a114d87c00
victorvisus <victor.vxg@gmail.com>
2026-03-16 10:31:49 +0100

    crear function generar usuario

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -20,11 +20,15 @@ export function getRandomNumber() {
 export function generarId() {
   const now = new Date();
   const year = String(now.getFullYear()).slice(-2);
-  const month = String(now.getMonth() + 1).padStart(2, "0"); //
-  const day = String(now.getDate()).padStart(2, "0");
-  const hour = String(now.getHours()).padStart(2, "0");
-  const minute = String(now.getMinutes()).padStart(2, "0");
+  const month = String(now.getMonth() + 1).padStart(2, '0'); //
+  const day = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const minute = String(now.getMinutes()).padStart(2, '0');
   const randomNumber = getRandomNumber();
 
   return `${year}${month}${day}${hour}${minute}${randomNumber}`;
+}
+
+export function generateUserName(usrName = 'usr', cod = getRandomNumber()) {
+  return `${usrName}${cod}`;
 }
